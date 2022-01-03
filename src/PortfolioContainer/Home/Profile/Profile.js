4a3d395c10463ff3e5accffba9ac144640cb82f1
Nischal <nischal.aremanda1432@gmail.com>
2022-01-03 12:50:24 +0530

    first commit

---
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Typical from "react-typical";
+import './Profile.css'
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-google-plus-square"></i>
+              </a>
+              <a href="https://www.instagram.com/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.youtube.com/">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm{" "}
+              <span className="highlighted-text">Nischal Aremanda</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Full Stack Web Developer",
+                    1000,
+                    "Angular",
+                    1000,
+                    "React",
+                    1000,
+                    "Python",
+                    1000,
+                    "Django",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back end
+                operations
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn"> Hire Me </button>
+            <a href="NISCHAL_RESUME.pdf" download="nischal NISCHAL_RESUME.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
